8693dd879713a5fb8e008ea961b8bb1673e54ae6
youngvita <youngvita@gmail.com>
2021-04-03 23:26:13 +0900

    주소 변경

--- src/SignUp.js
+++ src/SignUp.js
@@ -21,7 +21,7 @@ function Copyright() {
     return (
       <Typography variant="body2" color="textSecondary" align="center">
         {'Copyright © '}
-        <Link color="inherit" href="https://meetroom.web.app/">
+        <Link color="inherit" href="https://hoeuisil.web.app/">
           회의실예약관리
         </Link>{' '}
         {new Date().getFullYear()}
